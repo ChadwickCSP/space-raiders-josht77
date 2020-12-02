868eb67f5824b5ea79b94bcf571c88d4b7e62ec9
Joshua Thomas <jthomas2023@chadwickschool.org>
2020-12-01 20:14:18 -0800

    Milestone 1: commits of new work
    I have begun to work on milestone 1 and I finished the first 2 videos,
    however, I had some difficulty with the challenge for the first vide.
    I will look further into it tomorrow when I finish the milestone

---
+++ SpaceRaiders/Assets/Scripts/LaserController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    // specifies how fast the laser moves
+    public float speed;
+    // specifies how far off the screen the laser can be before it is destroyed
+    public float maxY, minY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Uses the speed and deltaTime to move the laser toward the top of the screen
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        float y = transform.position.y;
+
+        // Checks if the y position is outside of the upper bound and if it is it destroys it
+        if (y > maxY)
+        {
+            UnityEngine.Object.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ SpaceRaiders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // different variables
+    public float speed;
+    public float maxX, minX;
+    public float maxY, minY;
+
+    // The laser gameobject to clone when firing
+    public GameObject laser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+        handleFire();
+    }
+
+    private void handleFire()
+    {
+        // allows the player to use space bar to fire the laser
+        bool isFiring = Input.GetKeyDown(KeyCode.Space);
+
+        if(isFiring)
+        {
+            print("Fire!");
+            GameObject newLaser = UnityEngine.Object.Instantiate(laser);
+            float x, y;
+            x = transform.position.x;
+            y = transform.position.y + 0.5f;
+            newLaser.transform.position = new Vector2(x, y);
+
+            LaserController laserController = newLaser.GetComponent<LaserController>();
+            laserController.speed = 7;
+            // creates the object which is a laser allowing the ship to fire it
+        }
+    }
+    void movePlayer()
+    {
+        float playerInput = Input.GetAxis("Horizontal");
+        float playerYInput = Input.GetAxis("Vertical");
+
+        // allows for the player to control the movement of the ship with wasd
+        transform.Translate(Vector3.right * speed * Time.deltaTime * playerInput);
+        transform.Translate(Vector3.up * speed * Time.deltaTime * playerYInput);
+
+        // variables
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        // if the ship is past the minimum x value, it will print out "Beyond left side"
+        if(x < minX)
+        {
+            // will not allow the ship to move past the minX value
+            print("Beyond left side!");
+            transform.position = new Vector3(minX, y);
+        }
+
+        if(x > maxX)
+        {
+            // will not allow the ship to move past the maxX value
+            print("Beyond right side!");
+            transform.position = new Vector3(maxX, y);
+        }
+
+        if(y < minY)
+        {
+            print("Beyond bottom");
+            transform.position = new Vector3(minY, x);
+        }
+
+        if(y > maxY)
+        {
+            print("Beyond top");
+            transform.position = new Vector3(maxY, x);
+        }
+    }
+
+}
