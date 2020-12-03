795e19cd75e2f72f351ea4876c1ee0c47448b2e9
Joshua Thomas <jthomas2023@chadwickschool.org>
2020-12-02 18:58:03 -0800

    Milestone 1: committing new work
    I finished the third video of the first milestone and attempted again
    to fix my problem with the challenge in video one. However, I still had
    problems so I posted my code on piazza hoping I could get some help.

---
+++ SpaceRaiders/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Controls the speed of this enemy ship
+    public float speedX, speedY;
+    // Controls the bounds of the Y axis
+    public float minY, maxY;
+    // Controls the bounds of the X axis
+    public float minX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveEnemy();
+    }
+
+    void moveEnemy()
+    {
+        // Updates my enemys position on the screen
+        transform.Translate(new Vector2(speedX, speedY) * Time.deltaTime);
+
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        // Checks if the ship is out of bounds, if it is it destroys itself
+        if ( x < minX || x > maxX || y < minY || y > maxY )
+        {
+            UnityEngine.Object.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // This is the object that collided with the Enemy!
+        GameObject otherObject = collision.gameObject;
+
+        // Returns a LaserController if the otheObject contains one otherwise
+        // returns null
+        LaserController laserController = otherObject.GetComponent<LaserController>();
+
+        // If the otherObject has a LaserController, that means our EnemyShip should be destroyed
+        if ( laserController != null)
+        {
+            // Destroy both the enemy ship and the laser game obects
+            UnityEngine.Object.Destroy(this.gameObject);
+            UnityEngine.Object.Destroy(otherObject);
+        }
+    }
+}
---
+++ SpaceRaiders/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // The number of seconds between enemy spawns
+    public float spawnRate;
+
+    // the time at which the last spawn occurred
+    public float lastSpawnTime;
+
+    // This is the enemy to spawn
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastSpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = Time.time;
+        float elapsedTime = currentTime - lastSpawnTime;
+
+        if (elapsedTime > spawnRate)
+        {
+            print("Spawn");
+
+            // Create a new enemy instance and set its starting position
+            GameObject newEnemy = UnityEngine.Object.Instantiate(enemy);
+            newEnemy.transform.position = new Vector2(-10, 5);
+
+            // Set the speed of the enemy
+            EnemyController enemyController = newEnemy.GetComponent<EnemyController>();
+            enemyController.speedX = 5;
+            enemyController.speedY = -2;
+
+            lastSpawnTime = currentTime;
+        }
+    }
+}
